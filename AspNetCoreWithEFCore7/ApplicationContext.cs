591aef2e00ac3c54aab1bad2202db6c2bdd708e3
Maxim Eshenko <eshenko.maxim@gmail.com>
2023-06-15 16:10:13 +0300

    AspNetCoreWithEFCore7 project

---
+++ AspNetCoreWithEFCore7/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWithEFCore7;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();   // создаем бд с новой схемой
+    }
+}
---
+++ AspNetCoreWithEFCore7/Program.cs
@@ -0,0 +1,33 @@
+using AspNetCoreWithEFCore7;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// получаем строку подключения из файла конфигурации
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
